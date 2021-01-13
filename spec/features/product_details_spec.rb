3c4c7d39c9ae72c407345e3708a5906fb5e741bf
agxcd <agxcdcode@gmail.com>
2021-01-13 01:17:17 -0800

    Feature Spec: product testing

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -20,9 +20,11 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
   scenario "They see all products" do
     visit root_path
 
+    first('.product').click_link('Details')
+    
     # commented out b/c it's for debugging only
     # save_and_open_screenshot
 
-    expect(page).to have_css 'article.product', count: 10
+    expect(page).to have_css('.products-show')
   end
 end
